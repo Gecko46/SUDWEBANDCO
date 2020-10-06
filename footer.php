c3c71814faacf10e935ea7593a4ac3b4959ba330
Gecko46 <72340519+Gecko46@users.noreply.github.com>
2020-10-06 10:49:28 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,22 @@
+<!-- DEBUT FOOTER -->
+<footer>
+<div class="footer-cont">
+<div class="container">
+<div class="row">
+<div class="col-md color-perso-4">
+<p><b>FOOTER BLOC 1/3</b></p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan eleifend aliquet. Sed a arcu a erat elementum viverra. Aliquam hendrerit molestie lectus eget imperdiet. Nunc blandit ipsum orci, ac efficitur nulla eleifend ac. Nam sagittis, ligula ut hendrerit posuere, urna sem sagittis lectus, sollicitudin pulvinar urna ligula a urna. Maecenas vulputate blandit lacus, quis venenatis sapien dignissim sollicitudin. Aenean vel congue nibh, quis accumsan magna. In lobortis felis sit amet nisl feugiat, ut</p>
+</div>
+<div class="col-md color-perso-4">
+<p><b>FOOTER BLOC 2/3</b></p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan eleifend aliquet. Sed a arcu a erat elementum viverra. Aliquam hendrerit molestie lectus eget imperdiet. Nunc blandit ipsum orci, ac efficitur nulla eleifend ac. Nam sagittis, ligula ut hendrerit posuere, urna sem sagittis lectus, sollicitudin pulvinar urna ligula a urna.</p>
+</div>
+<div class="col-md color-perso-4">
+<p><b>FOOTER BLOC 3/3</b></p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan eleifend aliquet. Sed a arcu a erat elementum viverra. Aliquam hendrerit molestie lectus eget imperdiet. Nunc blandit ipsum orci, ac efficitur nulla eleifend ac. Nam sagittis, ligula ut hendrerit posuere, urna sem sagittis lectus, sollicitudin pulvinar urna ligula a urna. Maecenas vulputate blandit lacus, quis venenatis sapien dignissim sollicitudin. Aenean vel congue nibh, quis accumsan magna. In lobortis felis sit amet nisl feugiat, ut</p>
+</div>
+</div>
+</div>
+</div>
+</footer>
+<!-- FIN FOOTER -->
---
+++ index.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="fr">
+
+  <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <!-- bootstrap 4 responsive metatag -->
+    <title>Site test asso freelances</title>
+    <meta name="description" content="Description de ma page">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"> <!-- cdn bootstrap css -->
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+  </head>
+  
+<body>
+
+	
+<?php include("header.php"); ?>
+
+<!-- DEBUT BANNIERE -->
+<div class="home-top-banner">
+<h1>Les artisans du web</h1>
+<svg width="10em" height="10em" viewBox="0 0 16 16" class="bi bi-bounding-box" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M5 2V0H0v5h2v6H0v5h5v-2h6v2h5v-5h-2V5h2V0h-5v2H5zm6 1H5v2H3v6h2v2h6v-2h2V5h-2V3zm1-2v3h3V1h-3zm3 11h-3v3h3v-3zM4 15v-3H1v3h3zM1 4h3V1H1v3z"/>
+</svg>
+<h2>SLOGAN 2</h2>
+</div>
+<!-- FIN BANNIERE -->
+
+<div class="container-fluid">
+
+<!-- DEBUT COMPETENCES -->
+<div id="skills"></div> <!-- marqueur menu ancre -->
+<div class="row skills-cont">
+<div class="col-md-12">
+<h1>Nos compétences</h1>
+</div>
+<div class="col-md">
+<img src="img/skills-drupal-2.jpg" />
+</div>
+<div class="col-md">
+<img src="img/skills-wordpress-2.jpg" />
+</div>
+<div class="col-md">
+<img src="img/skills-prestashop-2.jpg" />
+</div>
+<div class="col-md">
+<img src="img/skills-woocommerce-2.jpg" />
+</div>
+<div class="col-md">
+<img src="img/skills-bootstrap-2.jpg" />
+</div>
+</div>
+<!-- FIN COMPETENCES -->
+
+<div class="row">
+<div class="col-md color-perso-1">
+<p><b>BLOC 1/3</b></p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan eleifend aliquet. Sed a arcu a erat elementum viverra. Aliquam hendrerit molestie lectus eget imperdiet. Nunc blandit ipsum orci, ac efficitur nulla eleifend ac. Nam sagittis, ligula ut hendrerit posuere, urna sem sagittis lectus, sollicitudin pulvinar urna ligula a urna. Maecenas vulputate blandit lacus, quis venenatis sapien dignissim sollicitudin. Aenean vel congue nibh, quis accumsan magna. In lobortis felis sit amet nisl feugiat, ut pellentesque mauris malesuada. Aliquam sit amet hendrerit ante. Donec cursus iaculis molestie. Sed eu justo sem. Cras convallis eget sem eu varius. Ut eget tempor ante, pellentesque ultricies nulla. Nulla et sapien scelerisque, viverra libero a, aliquam neque. Suspendisse placerat in ex id finibus. Aenean odio massa, pulvinar eu quam at, pretium mollis felis.</p>
+</div>
+<div class="col-md color-perso-2">
+<p><b>BLOC 2/3</b></p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan eleifend aliquet. Sed a arcu a erat elementum viverra. Aliquam hendrerit molestie lectus eget imperdiet. Nunc blandit ipsum orci, ac efficitur nulla eleifend ac. Nam sagittis, ligula ut hendrerit posuere, urna sem sagittis lectus, sollicitudin pulvinar urna ligula a urna.</p>
+</div>
+<div class="col-md color-perso-3">
+<p><b>BLOC 3/3</b></p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan eleifend aliquet. Sed a arcu a erat elementum viverra. Aliquam hendrerit molestie lectus eget imperdiet. Nunc blandit ipsum orci, ac efficitur nulla eleifend ac. Nam sagittis, ligula ut hendrerit posuere, urna sem sagittis lectus, sollicitudin pulvinar urna ligula a urna. Maecenas vulputate blandit lacus, quis venenatis sapien dignissim sollicitudin. Aenean vel congue nibh, quis accumsan magna. In lobortis felis sit amet nisl feugiat, ut pellentesque mauris malesuada. Aliquam sit amet hendrerit ante. Donec cursus iaculis molestie. Sed eu justo sem. Cras convallis eget sem eu varius. Ut eget tempor ante, pellentesque ultricies nulla. Nulla et sapien scelerisque, viverra libero a, aliquam neque. Suspendisse placerat in ex id finibus. Aenean odio massa, pulvinar eu quam at, pretium mollis felis.</p>
+</div>
+</div>
+
+<div class="row">
+<div class="col-md color-perso-3">
+<p><b>BLOC 1/3</b></p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan eleifend aliquet. Sed a arcu a erat elementum viverra. Aliquam hendrerit molestie lectus eget imperdiet. Nunc blandit ipsum orci, ac efficitur nulla eleifend ac. Nam sagittis, ligula ut hendrerit posuere, urna sem sagittis lectus, sollicitudin pulvinar urna ligula a urna. Maecenas vulputate blandit lacus, quis venenatis sapien dignissim sollicitudin. Aenean vel congue nibh, quis accumsan magna. In lobortis felis sit amet nisl feugiat, ut pellentesque mauris malesuada. Aliquam sit amet hendrerit ante. Donec cursus iaculis molestie. Sed eu justo sem. Cras convallis eget sem eu varius. Ut eget tempor ante, pellentesque ultricies nulla. Nulla et sapien scelerisque, viverra libero a, aliquam neque. Suspendisse placerat in ex id finibus. Aenean odio massa, pulvinar eu quam at, pretium mollis felis.</p>
+</div>
+<div class="col-md color-perso-1">
+<p><b>BLOC 2/3</b></p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan eleifend aliquet. Sed a arcu a erat elementum viverra. Aliquam hendrerit molestie lectus eget imperdiet. Nunc blandit ipsum orci, ac efficitur nulla eleifend ac. Nam sagittis, ligula ut hendrerit posuere, urna sem sagittis lectus, sollicitudin pulvinar urna ligula a urna.</p>
+</div>
+<div class="col-md color-perso-2">
+<p><b>BLOC 3/3</b></p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan eleifend aliquet. Sed a arcu a erat elementum viverra. Aliquam hendrerit molestie lectus eget imperdiet. Nunc blandit ipsum orci, ac efficitur nulla eleifend ac. Nam sagittis, ligula ut hendrerit posuere, urna sem sagittis lectus, sollicitudin pulvinar urna ligula a urna. Maecenas vulputate blandit lacus, quis venenatis sapien dignissim sollicitudin. Aenean vel congue nibh, quis accumsan magna. In lobortis felis sit amet nisl feugiat, ut pellentesque mauris malesuada. Aliquam sit amet hendrerit ante. Donec cursus iaculis molestie. Sed eu justo sem. Cras convallis eget sem eu varius. Ut eget tempor ante, pellentesque ultricies nulla. Nulla et sapien scelerisque, viverra libero a, aliquam neque. Suspendisse placerat in ex id finibus. Aenean odio massa, pulvinar eu quam at, pretium mollis felis.</p>
+</div>
+</div>
+
+<div class="row">
+<div class="col-md color-perso-1">
+<p><b>BLOC 1/3</b></p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan eleifend aliquet. Sed a arcu a erat elementum viverra. Aliquam hendrerit molestie lectus eget imperdiet. Nunc blandit ipsum orci, ac efficitur nulla eleifend ac. Nam sagittis, ligula ut hendrerit posuere, urna sem sagittis lectus, sollicitudin pulvinar urna ligula a urna. Maecenas vulputate blandit lacus, quis venenatis sapien dignissim sollicitudin. Aenean vel congue nibh, quis accumsan magna. In lobortis felis sit amet nisl feugiat, ut pellentesque mauris malesuada. Aliquam sit amet hendrerit ante. Donec cursus iaculis molestie. Sed eu justo sem. Cras convallis eget sem eu varius. Ut eget tempor ante, pellentesque ultricies nulla. Nulla et sapien scelerisque, viverra libero a, aliquam neque. Suspendisse placerat in ex id finibus. Aenean odio massa, pulvinar eu quam at, pretium mollis felis.</p>
+</div>
+<div class="col-md color-perso-2">
+<p><b>BLOC 2/3</b></p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan eleifend aliquet. Sed a arcu a erat elementum viverra. Aliquam hendrerit molestie lectus eget imperdiet. Nunc blandit ipsum orci, ac efficitur nulla eleifend ac. Nam sagittis, ligula ut hendrerit posuere, urna sem sagittis lectus, sollicitudin pulvinar urna ligula a urna.</p>
+</div>
+<div class="col-md color-perso-3">
+<p><b>BLOC 3/3</b></p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam accumsan eleifend aliquet. Sed a arcu a erat elementum viverra. Aliquam hendrerit molestie lectus eget imperdiet. Nunc blandit ipsum orci, ac efficitur nulla eleifend ac. Nam sagittis, ligula ut hendrerit posuere, urna sem sagittis lectus, sollicitudin pulvinar urna ligula a urna. Maecenas vulputate blandit lacus, quis venenatis sapien dignissim sollicitudin. Aenean vel congue nibh, quis accumsan magna. In lobortis felis sit amet nisl feugiat, ut pellentesque mauris malesuada. Aliquam sit amet hendrerit ante. Donec cursus iaculis molestie. Sed eu justo sem. Cras convallis eget sem eu varius. Ut eget tempor ante, pellentesque ultricies nulla. Nulla et sapien scelerisque, viverra libero a, aliquam neque. Suspendisse placerat in ex id finibus. Aenean odio massa, pulvinar eu quam at, pretium mollis felis.</p>
+</div>
+</div>
+
+</div>
+
+<?php include("footer.php"); ?>
+
+<!-- début cdn bootstrap bibliothèques jquery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="js/custom.js"></script>
+<!-- fin cdn bootstrap bibliothèques jquery -->
+	
+</body>
+  
+</html>
